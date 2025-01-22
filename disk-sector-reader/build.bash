3c945f4cc91cea3cf045c57d2b0c3b7570778322
Dawid Bielecki <dawciobiel@gmail.com>
2025-01-22 14:42:50 +0100

    Refactor script with minimal changes

--- disk-sector-reader/build.bash
+++ disk-sector-reader/build.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # The script is used for compilation and linking using NASM of the source code file given as the launch parameter.
+#
 # Author: Dawid Bielecki - dawciobiel
 # GitHub: https://github.com/dawciobiel
+#
 # Usage:
 #   ./build.bash <nasm_source_file>
 
